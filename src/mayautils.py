9630fd5c60e48dd45fec0e34d20e0bac4cb88623
jbdblack <65990035+jbdblack@users.noreply.github.com>
2020-07-02 17:51:51 -0500

    Initial commit of smartsaveui and updated mayautils

--- src/mayautils.py
+++ src/mayautils.py
@@ -7,15 +7,12 @@ log = logging.getLogger(__name__)
 
 class SceneFile(object):
     """This class represents a DCC software scene file
-
     The class will be a convenient object that we can use to manipulate our scene files.
-
     Attributes:
         dir (Path, optional): Directory to the scene file. Defaults to ''.
         descriptor (str, optional): Short descriptor of the scene file. Defaults to "main".
         version (int, optional): Version number. Defaults to 1.
         ext (str, optional): Extension. Defaults to "ma".
-
     """
 
     def __init__(self, dir='', descriptor='main', version=1, ext="ma"):
@@ -42,22 +39,17 @@ class SceneFile(object):
 
     def path(self):
         """The function returns a path to scene file.
-
         This includes the drive letter, any directory path and the file name.
-
         Returns:
             Path: The path to the scene file.
-
         """
         return Path(self.dir) / self.basename()
 
 
     def save(self):
         """Saves the scene file.
-
         Returns:
             :obj:'Path': The path to the scene file if successful, None, otherwise.
-
         """
         try:
             pmc.system.saveAs(self.path())
@@ -65,3 +57,6 @@ class SceneFile(object):
             log.warning("Missing directories. Creating directories...")
             self.dir.makedirs_p()
             pmc.system.saveAs(self.path())
+
+    def increment_and_save(self):
+        pass
